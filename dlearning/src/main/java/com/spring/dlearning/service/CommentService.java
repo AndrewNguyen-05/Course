f33c039072a648c45b1cf924c9851994197392ce
duclk <lekhanhduccc@gmail.com>
2024-09-14 04:42:00 +0700

    edit and delete comment

--- dlearning/src/main/java/com/spring/dlearning/service/CommentService.java
+++ dlearning/src/main/java/com/spring/dlearning/service/CommentService.java
@@ -1,13 +1,17 @@
 package com.spring.dlearning.service;
 
 import com.spring.dlearning.dto.request.CommentRequest;
+import com.spring.dlearning.dto.request.UpdateCommentRequest;
 import com.spring.dlearning.dto.response.CommentResponse;
+import com.spring.dlearning.dto.response.DeleteCommentResponse;
+import com.spring.dlearning.dto.response.UpdateCommentResponse;
 import com.spring.dlearning.entity.Comment;
 import com.spring.dlearning.entity.Course;
 import com.spring.dlearning.entity.User;
 import com.spring.dlearning.exception.AppException;
 import com.spring.dlearning.exception.ErrorCode;
 import com.spring.dlearning.mapper.CommentMapper;
+import com.spring.dlearning.mapper.ProfileMapper;
 import com.spring.dlearning.repository.CommentRepository;
 import com.spring.dlearning.repository.CourseRepository;
 import com.spring.dlearning.repository.UserRepository;
@@ -18,6 +22,7 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -66,4 +71,46 @@ public class CommentService {
         return commentMapper.toCommentResponse(newComment);
     }
 
+    public DeleteCommentResponse deleteCommentById(Long id) {
+        String email = SecurityUtils.getCurrentUserLogin()
+                .orElseThrow(() -> new AppException(ErrorCode.EMAIL_INVALID));
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.COMMENT_NOT_EXISTED));
+
+        if (Objects.equals(user.getId(), comment.getUser().getId())) {
+            commentRepository.deleteById(id);
+            return DeleteCommentResponse.builder()
+                    .id(id)
+                    .message("Delete Comment Successfully")
+                    .build();
+        }
+
+        throw new AppException(ErrorCode.DELETE_COMMENT_ISVALID);
+    }
+
+    public UpdateCommentResponse updateComment(Long id, UpdateCommentRequest request){
+        String email = SecurityUtils.getCurrentUserLogin()
+                .orElseThrow(() -> new AppException(ErrorCode.EMAIL_INVALID));
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        Comment comment = commentRepository.findById(id)
+                .orElseThrow(() -> new AppException(ErrorCode.COMMENT_NOT_EXISTED));
+
+        if(Objects.equals(user.getId(), comment.getUser().getId())) {
+            commentMapper.toComment(request,comment);
+            commentRepository.save(comment);
+            return UpdateCommentResponse.builder()
+                    .id(comment.getId())
+                    .content(comment.getContent())
+                    .build();
+        }
+        throw new AppException(ErrorCode.UPDATE_COMMENT_ISVALID);
+
+    }
 }
